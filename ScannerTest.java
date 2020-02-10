93d9f919d4df4998210b22c5b7d714072223c5da
ThCau4706 <60898479+ThCau4706@users.noreply.github.com>
2020-02-10 15:05:59 -0500

    Add files via upload

---
+++ ScannerTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scannertest;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author thcau4706
+ */
+public class ScannerTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int age =0;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Please enter your age: ");
+        age = input.nextInt();
+        System.out.println("Hi your age is "+ age );
+    }
+    
+}
